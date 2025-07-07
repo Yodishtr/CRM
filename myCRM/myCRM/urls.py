063a7dacf7b8ced6b50a63b05ce6cf25aa346e6a
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-07-07 00:20:07 -0400

    a lot of work

--- myCRM/myCRM/urls.py
+++ myCRM/myCRM/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib.auth import views
 from core.views import index, about
-from userprofile.views import signup
+from userprofile.views import signup, custom_logout
 
 urlpatterns = [
     path('', index, name='index'),
+    path('dashboard/leads/', include('lead.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('about/', about, name='about'),
     path('sign-up/', signup, name='signup'),
+    path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', custom_logout, name='logout'),
     path('admin/', admin.site.urls),
 
 ]
