cc7447d5ecfced1f7fe4e47e6473c853065e0b95
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-07-12 02:38:10 -0400

    ok

--- myCRM/myCRM/urls.py
+++ myCRM/myCRM/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
 from core.views import index, about
-from userprofile.views import signup, custom_logout
+from userprofile.views import signup, custom_logout, myaccount
 
 urlpatterns = [
     path('', index, name='index'),
     path('dashboard/leads/', include('lead.urls')),
     path('dashboard/clients/', include('client.urls')),
+    path('dashboard/myacccount/', myaccount, name='myaccount'),
+    path('dashboard/teams/', include('team.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('about/', about, name='about'),
     path('sign-up/', signup, name='signup'),
--- myCRM/userprofile/views.py
+++ myCRM/userprofile/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from .models import Userprofile
+from team.models import Team
 
 def signup(request):
     if request.method == 'POST':
@@ -9,11 +11,20 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             Userprofile.objects.create(user=user)
+            team = Team.objects.create(name="Team Name", created_by=request.user)
+            team.members.add(request.user)
             return redirect('/log-in/')
     else:
         form = UserCreationForm()
     return render(request, 'userprofile/signup.html', {
         'form': form
+    })
+
+@login_required
+def myaccount(request):
+    team = Team.objects.filter(created_by=request.user)[0]
+    return render(request, 'userprofile/myaccount.html', {
+        'team': team
     })
 
 def custom_logout(request):
